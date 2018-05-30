4aab1c660485109091e785b12e8a093fe5f967cf
Frank Yu <frank123111@gmail.com>
2018-05-30 15:39:20 +0800

    train_yolo

--- train_my_own_yolo.py
+++ train_my_own_yolo.py
@@ -3,7 +3,16 @@ import PIL
 from PIL import Image
 import numpy as np
 import argparse
-# import keras
+# import sys
+# sys.path.append('/usr/local/bin/cuda-9.0/lib64')
+import tensorflow as tf
+import keras
+from keras import backend as K
+from keras.layers import Input, Lambda, Conv2D
+from keras.models import load_model, Model
+from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
+from yad2k.models.keras_yolo import (yolo_body, yolo_eval, yolo_head, yolo_loss)
+from yad2k.utils.draw_boxes import draw_boxes
 
 def parse_args():
     '''
@@ -13,8 +22,12 @@ def parse_args():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-i', '--image_path')
     argparser.add_argument('-l', '--label_path')
+    argparser.add_argument('-a', '--anchors_path')
+    argparser.add_argument('-c', '--class_names')
+
     argparser.add_argument('-s', '--starting_file', default=0)
-    argparser.add_argument('-b', '--batch_size', default=1000)
+    argparser.add_argument('-b', '--batch_size', default=900)
+    argparser.add_argument('-m', '--max_batches', default=None)
     argparser.add_argument('-r', '--regions', default=[13, 13])
     args = argparser.parse_args()
     return args
@@ -35,10 +48,11 @@ def process_data(image_path, label_path, starting_file, batch_size, regions):
     all_labels = []
     fns = os.listdir(image_path)
     max_labels = 0
+
     for fn in fns[starting_file: starting_file+batch_size]:
         labels = []
         images.append(Image.open(image_path+fn))
-        txt_fn = label_path + fn.split('.')[0] + '.txt'
+        txt_fn = str(label_path) + str(fn.split('.')[0]) + '.txt'
         with open(txt_fn, 'r') as f:
             label_txt = f.read()
             lines = label_txt.split('\n\n')
@@ -119,7 +133,7 @@ def preprocess_true_boxes(true_boxes, anchors, regions):
     num_anchors = len(anchors)
     num_box_params = true_boxes.shape[1]
     conv_height, conv_width = regions
-    detector_mask = np.zeros((conv_height, conv_width, num_anchors, num_box_params), dtype=np.float32)
+    detector_mask = np.zeros((conv_height, conv_width, num_anchors, 1), dtype=np.float32)
     matching_true_boxes = np.zeros((conv_height, conv_width, num_anchors, num_box_params), dtype=np.float32)
     for box in true_boxes:
         cls = box[4:5]
@@ -147,20 +161,225 @@ def preprocess_true_boxes(true_boxes, anchors, regions):
 
         if best_iou > 0:
             detector_mask[i, j, best_anchor] = 1
+            adjusted_box = np.array([box[0]-j, box[1]-i,
+                                     np.log(box[2]/anchors[best_anchor][0]),
+                                     np.log(box[3]/anchors[best_anchor][1]), cls],
+                                    dtype=np.float32)
+            matching_true_boxes[i, j, best_anchor] = adjusted_box
+    return detector_mask, matching_true_boxes
 
+def create_model(anchors, class_names, regions, load_pretrained=True, freeze_body=True):
+    '''
+    create the model
+    :param anchors:
+    :param class_names:
+    :param regions :type list
+    :param num_anchors
+    :param load_pretrained:
+    :param freeze_body:
+    :return: YOLO v2 with new output layers
+             Yolo v2 with custom loss Lambda Layer
+    '''
+    conv_x, conv_y = regions
+    num_anchors = len(anchors)
+    x_shape, y_shape = conv_x * 32, conv_y * 32
+    detectors_mask_shape = (conv_x, conv_y, 5, 1)
+    matching_boxes_shape = (conv_x, conv_y, 5, num_anchors)
+
+    # Create model input layers
+    image_input = Input(shape=(x_shape, y_shape, 3))
+    boxes_input = Input(shape=(None, 5))
+    detectors_mask_input = Input(shape=detectors_mask_shape)
+    matching_boxes_input = Input(shape=matching_boxes_shape)
+
+    # Create model body
+    yolo_model = yolo_body(image_input, len(anchors), len(class_names))
+    topless_yolo = Model(yolo_model.input, yolo_model.layers[-2].output)
+
+    if load_pretrained:
+        topless_yolo_path = os.path.join('model_data', 'yolo_topless.h5')
+        if not os.path.exists(topless_yolo_path):
+            print("Create topless weights file first")
+            yolo_path = os.path.join('model_data', 'yolo.h5')
+            model_body = load_model(yolo_path)
+            model_body = Model(model_body.inputs, model_body.layers[-2].output)
+            model_body.save_weights(topless_yolo_path)
+        topless_yolo.load_weights(topless_yolo_path)
+
+    if freeze_body:
+        for layer in topless_yolo.layers:
+            layer.trainable = False
+    final_layer = Conv2D(len(anchors)*(5+len(class_names)), (1, 1), activation='linear')(topless_yolo.output)
+
+    model_body = Model(image_input, final_layer)
+
+    with tf.device('/cpu:0'):
+        model_loss = Lambda(yolo_loss,
+                            output_shape=(1,),
+                            name='yolo_loss',
+                            arguments={'anchors': anchors,
+                                       "num_classes":len(class_names)})(
+            [model_body.output, boxes_input, detectors_mask_input, matching_boxes_input])
+    model = Model([model_body.input, boxes_input, detectors_mask_input, matching_boxes_input], model_loss)
+
+    return model_body, model
+
+# def model_loss():
+
+
+def train(model, class_names, anchors, image_data, boxes, detectors_mask, matching_true_boxes, regions, validation_split=0.1):
+    '''
+
+    :param model:
+    :param class_names:
+    :param anchors:
+    :param image_data:
+    :param boxes:
+    :param detectors_mask:
+    :param matching_true_boxes:
+    :param validation_split:
+    :return:
+    '''
+    model.compile(optimizer='adam', loss={'yolo_loss':lambda y_true, y_pred: y_pred})
+    logging = TensorBoard()
+    checkpoint = ModelCheckpoint("trained_stage_3_best.h5", monitor='val_loss',
+                                 save_weights_only=True, save_best_only=True)
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=15, verbose=1, mode='auto')
+    model.fit([image_data, boxes, detectors_mask, matching_true_boxes], np.zeros(len(image_data)), validation_split=validation_split, batch_size=32, epochs=5, callbacks=[logging])
+    model.save_weights('trained_stage_1.h5')
+
+    model_body, model = create_model(anchors, class_names, regions, load_pretrained=False, freeze_body=False)
+    model.load_weights('trained_stage_1.h5')
+    model.compile(optimizer='adam', loss={'yolo_loss':lambda y_true, y_pred: y_pred})
+    model.fit([image_data, boxes, detectors_mask, matching_true_boxes],
+              np.zeros(len(image_data)),
+              validation_split=validation_split,
+              batch_size=None,
+              epochs=30,
+              callbacks=[logging])
+    model.save_weights('trained_stage_2.h5')
+
+    model.fit([image_data, boxes, detectors_mask, matching_true_boxes],
+              np.zeros(len(image_data)),
+              validation_split=validation_split,
+              batch_size=None,
+              epochs=8,
+              callbacks=[logging, checkpoint, early_stopping])
+    model.save_weights('trained_stage_3.h5')
+
+def draw(model_body, class_names, anchors, image_data, image_set='val',
+            weights_name='trained_stage_3_best.h5', out_path="output_images", save_all=True):
+    '''
+    Draw bounding boxes on image data
+    '''
+    if image_set == 'train':
+        image_data = np.array([np.expand_dims(image, axis=0)
+            for image in image_data[:int(len(image_data)*.9)]])
+    elif image_set == 'val':
+        image_data = np.array([np.expand_dims(image, axis=0)
+            for image in image_data[int(len(image_data)*.9):]])
+    elif image_set == 'all':
+        image_data = np.array([np.expand_dims(image, axis=0)
+            for image in image_data])
+    else:
+        ValueError("draw argument image_set must be 'train', 'val', or 'all'")
+    # model.load_weights(weights_name)
+    print(image_data.shape)
+    model_body.load_weights(weights_name)
+
+    # Create output variables for prediction.
+    yolo_outputs = yolo_head(model_body.output, anchors, len(class_names))
+    input_image_shape = K.placeholder(shape=(2, ))
+    boxes, scores, classes = yolo_eval(
+        yolo_outputs, input_image_shape, score_threshold=0.07, iou_threshold=0.0)
+
+    # Run prediction on overfit image.
+    sess = K.get_session()  # TODO: Remove dependence on Tensorflow session.
+
+    if  not os.path.exists(out_path):
+        os.makedirs(out_path)
+    for i in range(len(image_data)):
+        out_boxes, out_scores, out_classes = sess.run(
+            [boxes, scores, classes],
+            feed_dict={
+                model_body.input: image_data[i],
+                input_image_shape: [image_data.shape[2], image_data.shape[3]],
+                K.learning_phase(): 0
+            })
+        print('Found {} boxes for image.'.format(len(out_boxes)))
+        print(out_boxes)
+
+        # Plot image with predicted boxes.
+        image_with_boxes = draw_boxes(image_data[i][0], out_boxes, out_classes,
+                                    class_names, out_scores)
+        # Save the image:
+        if save_all or (len(out_boxes) > 0):
+            image = PIL.Image.fromarray(image_with_boxes)
+            image.save(os.path.join(out_path,str(i)+'.png'))
+
+        # To display (pauses the program):
+        # plt.imshow(image_with_boxes, interpolation='nearest')
+        # plt.show()
+
+def get_anchors(anchors_path, region):
+    with open(anchors_path, 'r') as f:
+        txt = f.read()
+        f.close()
+    anchor_pairs = txt.split(', ')
+    anchors = []
+    for anchor_pair in anchor_pairs:
+        anchor = np.array(anchor_pair.split(' '), dtype=np.float32)
+        anchor = anchor * np.array(region)
+        anchors.append(anchor)
+    return np.array(anchors)
+
+def get_max_batches(image_path, batch_size):
+    total_file_count = len(os.listdir(image_path))
+    batches, residual = divmod(total_file_count, batch_size)
+    if residual == 0:
+        return batches
+    return batches + 1
+
+def get_class_names(class_name_path):
+    with open(class_name_path, 'r') as f:
+        classes = f.read()
+        f.close()
+    return classes.split('\n')[:-1]
+
+def get_regions(region):
+    regions = args.regions
+    regions = regions.split('|')
+    regions = [int(i) for i in regions]
+    return regions
 
 def main():
     args = parse_args()
     image_path = args.image_path
     label_path = args.label_path
+    class_names = get_class_names(args.class_names)
     starting_file = args.starting_file
-    batch_size = args.batch_size
-    regions = args.regions
+    batch_size = int(args.batch_size)
+    regions = get_regions(args.regions)
+    anchors_path = args.anchors_path
+    max_batches = int(args.max_batches)
 
-    anchors = None
-    processed_images, processed_labels = process_data(image_path, label_path, starting_file, batch_size, regions)
-    get_detector_mask(processed_labels, anchors, regions)
+    anchors = get_anchors(anchors_path, regions)
+    if not max_batches:
+        max_batches = get_max_batches(image_path, batch_size)
+    for i in range(max_batches):
+        if i == 0:
+            model_body, model = create_model(anchors, class_names, regions)
+        else:
+            model_body, model = create_model(anchors, class_names, regions, load_pretrained=False, freeze_body=False)
+            model.load_weights('trained_stage_3_best.h5')
+        processed_images, processed_labels = process_data(image_path, label_path, starting_file+i*batch_size, batch_size, regions)
+        detectors_mask, matching_true_boxes = get_detector_mask(processed_labels, anchors, regions)
+        train(model, class_names, anchors, processed_images, processed_labels,
+              detectors_mask, matching_true_boxes, regions)
 
+
+    draw(model_body, class_names, anchors, processed_images,
+         image_set='val', weights_name='trained_stage_3_best.h5', save_all=False)
 
 if __name__ == "__main__":
     main()
---
+++ train_my_yolo.sh
@@ -0,0 +1,1 @@
+python train_my_own_yolo.py -i /media/frank/Storage/Project/Kaggle/WAD/input/train_color/ -l /media/frank/Storage/Project/Kaggle/WAD/input/train_label_txts/ -a /media/frank/Storage/Project/Kaggle/WAD/input/anchors.txt -c /media/frank/Storage/Project/Kaggle/WAD/input/class_names.txt -b 30 -m 2 - r 13|13
